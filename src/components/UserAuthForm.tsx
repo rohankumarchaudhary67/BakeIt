4989682dd2fb6eb8ce1aabb7b6ed9a17d4d8213c
rohankumarchaudhary67 <rohankumarchaudhary67@gmail.com>
2023-06-25 22:51:01 +0530

    Google Button Added in Sign In

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,15 +1,37 @@
+"use client"
+
 import { Button } from "./ui/Button"
 import { cn } from "@/lib/utils"
-import {FC} from 'react'
+import { FC, useState } from 'react'
+import {signIn} from "next-auth/react"
+import { Icons } from "./Icons"
 
 interface UserAuthFormProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
 
 }
 
-const UserAuthForm : FC<UserAuthFormProps> = ({ className, ...props}) => {
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const loadingWithGoogle = async () => {
+        setIsLoading(true);
+
+        try {
+            await signIn('google');
+        } catch (error) {
+            // Toast Notifciation
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <div className={cn('flex justify-center', className)} {...props}>
-            <Button size={'sm'} className="w-full">Google</Button>
+            <Button onClick={loadingWithGoogle} isLoading={isLoading} size={'sm'} className="w-full">
+                {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
+                Google
+            </Button>
         </div>
     )
 }
